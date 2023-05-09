48ea0063bdaadac2ab645db11f673c606d2d7524
FierceLinkle <73249228+FierceLinkle@users.noreply.github.com>
2023-05-09 13:32:31 +0100

    Initial commit

---
+++ Assets/Scripts/Gachapon.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gachapon : MonoBehaviour
+{
+    public GameObject CapsuleToy;
+
+    public GameObject[] capsuleArr;
+    public Transform[] displayArr;
+    int displayToggle;
+
+    bool inUse = false;
+    bool isEmpty = false;
+
+    float delay = 3f;
+
+    int index;
+
+    public TextMeshProUGUI EmptyText;
+    public TextMeshProUGUI CapsuleLeftText;
+    private int CapsuleLeft;
+
+    public Transform CameraShift;
+    bool hasShifted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inUse = false;
+        displayToggle = 0;
+        CapsuleLeft = capsuleArr.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space)) && inUse == false && isEmpty == false)
+        {
+            inUse = true;
+            GameObject cloneC = (GameObject)Instantiate(CapsuleToy);
+            //GameObject cloneT = (GameObject)Instantiate(capsuleArr[CapsuleSelect()]);
+            SpawnDisplay();
+            StartCoroutine(Buffer());
+            Destroy(cloneC, delay);
+            //Destroy(cloneT, delay);
+            RemoveCapsuleFromPool();
+            EmptyMachineCheck();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            CameraMove();
+        }
+
+        CapsuleLeftText.text = "Capsules Left: " + CapsuleLeft.ToString();
+    }
+
+    IEnumerator Buffer()
+    {
+        yield return new WaitForSeconds(delay - 0.5f);
+        GameObject cloneT = (GameObject)Instantiate(capsuleArr[CapsuleSelect()]);
+        Destroy(cloneT, delay);
+        yield return new WaitForSeconds(delay + 0.2f);
+        inUse = false;
+        CapsuleLeft--;
+    }
+
+    private int CapsuleSelect()
+    {
+        index = Random.Range(0, capsuleArr.Length);
+
+        if (capsuleArr[index] == null)
+        {
+            CapsuleSelect();
+        }
+
+        return index;
+    }
+
+    private void RemoveCapsuleFromPool()
+    {
+        capsuleArr[index] = null;
+    }
+
+    private void EmptyMachineCheck()
+    {
+        isEmpty = true;
+
+        for (int i = 0; i < capsuleArr.Length; i++)
+        {
+            if (capsuleArr[i] != null)
+            {
+                isEmpty = false;
+            }
+        }
+
+        if (isEmpty == true)
+        {
+            StartCoroutine(EmptyBuffer());
+        }
+    }
+
+    IEnumerator EmptyBuffer()
+    {
+        yield return new WaitForSeconds(delay + 0.2f);
+        EmptyText.text = "Machine Empty!";
+    }
+
+    public void CameraMove()
+    {
+        if (hasShifted == false)
+        {
+            CameraShift.position += new Vector3(20, 3, 0);
+            CameraShift.rotation = Quaternion.Euler(20, 180, 0);
+            hasShifted = true;
+        }
+        else
+        {
+            CameraShift.position -= new Vector3(20, 3, 0);
+            CameraShift.rotation = Quaternion.Euler(0, 180, 0);
+            hasShifted = false;
+        }
+    }
+
+    public void SpawnDisplay()
+    {
+        GameObject cloneD = (GameObject)Instantiate(capsuleArr[index], displayArr[displayToggle].position + new Vector3(0, 0.75f, 0), transform.rotation);
+        cloneD.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+        displayToggle++;
+    }
+}
---
+++ Assets/Scripts/SummonMechanics.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SummonMechanics : MonoBehaviour
+{
+    public Image SummonResult;
+    public Button SummonButton;
+    public TextMeshProUGUI Results;
+    public GameObject Full;
+
+    private float Value;
+    public string[] ResultsDisplay;
+    private string ResultsValue;
+    private int ResultsIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Results.text = "Results:" + "\n" + ResultsDisplay[0] + "\n" + ResultsDisplay[1] + "\n" + ResultsDisplay[02] + "\n" + ResultsDisplay[3] + "\n" + ResultsDisplay[4] + "\n" + ResultsDisplay[5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Results.text = "Results:" + "\n" + ResultsDisplay[0] + "\n" + ResultsDisplay[1] + "\n" + ResultsDisplay[02] + "\n" + ResultsDisplay[3] + "\n" + ResultsDisplay[4] + "\n" + ResultsDisplay[5];
+    }
+
+    public void SummonSequence()
+    {
+        if (ResultsIndex < 5)
+        {
+            Value = Random.Range(0f, 1f);
+
+            if (Value <= 0.2f)
+            {
+                SummonResult.color = Color.green;
+                ResultsValue = "Green";
+            }
+            else if (Value > 0.2f && Value <= 0.4f)
+            {
+                SummonResult.color = Color.red;
+                ResultsValue = "Red";
+            }
+            else if (Value > 0.4f && Value <= 0.6f)
+            {
+                SummonResult.color = Color.blue;
+                ResultsValue = "Blue";
+            }
+            else if (Value > 0.6f && Value <= 0.8f)
+            {
+                SummonResult.color = Color.yellow;
+                ResultsValue = "Yellow";
+            }
+            else
+            {
+                SummonResult.color = Color.cyan;
+                ResultsValue = "Cyan";
+            }
+            ResultsDisplay[ResultsIndex] = ResultsValue;
+
+            ResultsIndex++;
+        } else
+        {
+            Full.SetActive(true);
+        }       
+
+    }
+}
